70662216cc9bc1c74e10cd6184ca5f6b6b0f8b08
rumple <ruipeng.wu@cyclone-robotics.com>
2021-09-02 10:56:17 +0800

    update

--- document.go
+++ document.go
@@ -21,21 +21,26 @@ type IDocument interface {
 
 type Document struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	CreatedAt time.Time          `bson:"created_at,omitempty" json:"created_at,omitempty"`
-	UpdatedAt time.Time          `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
-	DeletedAt time.Time          `bson:"deleted_at,omitempty" json:"-"`
+	CreatedAt *time.Time         `bson:"created_at,omitempty" json:"created_at,omitempty"`
+	UpdatedAt *time.Time         `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
+	DeletedAt *time.Time         `bson:"deleted_at,omitempty" json:"-"`
 }
 
 func (d *Document) PreCreate() {
 	d.ID = primitive.NewObjectID()
-	d.CreatedAt = time.Now().UTC().Truncate(time.Millisecond)
-	d.UpdatedAt = time.Now().UTC().Truncate(time.Millisecond)
+	d.CreatedAt = Now()
+	d.UpdatedAt = Now()
 }
 
 func (d *Document) PreUpdate() {
-	d.UpdatedAt = time.Now().UTC().Truncate(time.Millisecond)
+	d.UpdatedAt = Now()
 }
 
 func (d *Document) PreDelete() {
-	d.DeletedAt = time.Now().UTC().Truncate(time.Millisecond)
+	d.DeletedAt = Now()
+}
+
+func Now() *time.Time {
+	now := time.Now().UTC().Truncate(time.Millisecond)
+	return &now
 }
